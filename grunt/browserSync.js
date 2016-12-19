39e3608fef2929a531361b9afadd6a3090fb59e2
Elvis Henrique Pereira <elvishp2006@gmail.com>
2016-12-19 12:03:24 -0200

    Silent script

--- grunt/browserSync.js
+++ grunt/browserSync.js
@@ -1,7 +1,7 @@
 module.exports = {
 	dev: {
 		options: {
-			proxy: exec('wp option get siteurl').stdout,
+			proxy: exec('wp option get siteurl', {silent:true}).stdout,
 			files: ['style.css', 'assets/javascripts/built.js', '**/*.php'],
 			watchTask: true,
 			logConnections: true,

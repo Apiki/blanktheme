84c496b358fa273f48c743674826f001026f7492
Elvis Henrique <elvishp2006@gmail.com>
2017-04-05 11:13:54 -0300

    Default functions

--- searchform.php
+++ searchform.php
@@ -4,13 +4,14 @@ if ( ! function_exists( 'add_action' ) ) {
 	exit( 0 );
 }
 
-use GB\Theme\Helper\Utils;
 use GB\Theme\Core;
 ?>
-<form role="search" method="get" class="search-form" action="<?php echo esc_url( Utils::get_site_url() ); ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<input type="search" class="search-field" placeholder="<?php esc_attr_e( 'Type here to search:', Core::SLUG ); ?>" value="<?php the_search_query(); ?>" name="s" required />
+		<input type="search" class="search-form-field" placeholder="<?php esc_attr_e( 'Type here to search:', Core::SLUG ); ?>" value="<?php the_search_query(); ?>" name="s" required/>
 	</label>
 
-	<input type="submit" class="search-submit btn-secondary" value="<?php esc_html_e( 'Search', Core::SLUG ); ?>">
+	<button class="search-form-btn" type="submit" value="<?php esc_html_e( 'Search', Core::SLUG ); ?>">
+		<i class="icon-g-search"></i>
+	</button>
 </form>
